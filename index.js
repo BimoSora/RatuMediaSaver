f8c4498e1f168bcb1c599f977bd5d92dd1d6df1e
BimoSora <bimosora3@gmail.com>
2022-01-15 14:44:25 +0700

    first commit

--- index.js
+++ index.js
@@ -45,7 +45,10 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => 
       const stream = got.stream(url)
       stream
       .on('error', () => ctx.telegram.sendMessage(ctx.chat.id, 'An error has occurred'))
-      .on('progress', p => console.log(p))
+      .on("downloadProgress", ({ transferred, total, percent }) => {
+        const percentage = Math.round(percent * 100);
+        console.error(`progress: ${transferred}/${total} (${percentage}%)`);
+      })
       .on('data', chunk => buffer.push(chunk))
       .on('end', async () => {
         await ctx.telegram.sendDocument(ctx.chat.id, Buffer.concat(buffer),{
