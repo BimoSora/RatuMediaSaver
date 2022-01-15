262d6f8b5fb69f881904b1f4ba902cd40a793f30
BimoSora <bimosora3@gmail.com>
2022-01-15 07:15:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,24 +29,27 @@ function fromid(ctx){
 // bot.generateSession() // aktifkan ini untuk menghasilkan sesi dan nonaktifkan bot.run().
 
 bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => {
+  const startTime = Date.now() // <-- see this
   if(ctx.from.id == Number(process.env.ADMIN) || ctx.from.id == Number(process.env.ADMIN1) || ctx.from.id == Number(process.env.ADMIN2) || ctx.from.id == Number(process.env.ADMIN3) || ctx.from.id == Number(process.env.ADMIN4)){
     const url = ctx.text.replace('/url', '').trim();
     if (!url.length) return ctx.telegram.sendMessage(ctx.chat.id, 'No valid url found ')
     await ctx.telegram.sendMessage(ctx.chat.id,'Upload start')
     const buffer = await got(url).buffer()
-    const {ext, mime } = await FileType.fromBuffer(buffer)
+    const sizeFile = buffer.byteLength; // <-- see this
+    const { ext, mime } = await FileType.fromBuffer(buffer)
     let filename2 = ``;
     try {
       filename2 = new URL(url).pathname.split('/').pop();
     } catch (e) {
         console.error(e);
     }
-    const allowedFileFormats = ['mp4', 'mp3', 'rar', '7z', 'zip', 'png', 'jpeg', 'jpg', 'gif']
+    const allowedFileFormats = ['mov', 'webp', 'm4v', 'avi, 'flv', 'mkv', 'webm', '3gp', 'mpeg', 'mp4', 'mp3', 'pdf', 'rar', '7z', 'zip', 'png', 'jpeg', 'jpg', 'gif']
     if (allowedFileFormats.includes(ext)) {
         await ctx.telegram.sendDocument(ctx.chat.id,buffer,{
           fileName : filename2
         })
-        await ctx.telegram.sendMessage(ctx.chat.id,'Upload successful')
+        const processTime = Date.now() - startTime; // <-- see this
+        await ctx.telegram.sendMessage(ctx.chat.id,`Upload successful.\nSize file: ${bytes(sizeFile)}\nProcess time: ${processTime}ms`) // <-- and this
      } else {
         ctx.reply('File type not allowed')
      }
