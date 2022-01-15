7761ee6f5e5a19cf9975d69d8a3a8be8588288a6
BimoSora <bimosora3@gmail.com>
2022-01-15 23:29:49 +0700

    first commit

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => 
         await ctx.telegram.sendMedia(ctx.chat.id,Buffer.concat(buffer),{
           fileName : filename,
           TypeInputMedia:{
-            file : TypeInputFile
+            file : InputMediaDocument
           }
         })
         await ctx.telegram.sendMessage(ctx.chat.id,`Name: ${filename}`)
