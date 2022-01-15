edb7b1362d3a17fc3593690c521c6607891b7ebb
BimoSora <bimosora3@gmail.com>
2022-01-16 05:52:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => 
       .on('progress', p => console.log(p))
       .on('data', chunk => buffer.push(chunk))
       .on('end', async () => {
-        await ctx.telegram.sendMedia(ctx.chat.id,new Api.InputMediaDocument({
+        await ctx.telegram.sendMedia(ctx.chat.id,new InputMediaDocument(args: { id: TypeInputDocument; ttlSeconds?: number; query?: string }): InputMediaDocument({
           media: Buffer.concat(buffer),
           fileName: filename
         }))
