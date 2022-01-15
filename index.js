8c54440f5b450987917bf1a293ddcda441fb9c6a
BimoSora <bimosora3@gmail.com>
2022-01-15 22:55:08 +0700

    first commit

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => 
       .on('progress', p => console.log(p))
       .on('data', chunk => buffer.push(chunk))
       .on('end', async () => {
-        await ctx.telegram.sendDocument(ctx.chat.id,Buffer.concat(buffer),`Name: ${filename}`,{
-          fileName : filename
+        await ctx.telegram.sendDocument(ctx.chat.id,Buffer.concat(buffer),{
+          fileName : filename,
         })
         await ctx.telegram.sendMessage(ctx.chat.id,`Name: ${filename}`)
         await ctx.telegram.sendMessage(ctx.chat.id,`Upload successful`)
