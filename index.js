a00622e67074933dc46a7d9fd79ae290e61b486d
BimoSora <bimosora3@gmail.com>
2022-01-16 05:30:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // index.js
 require('dotenv').config();
-const {Snake} = require("tgsnake");
+const {Snake,GramJs} = require("tgsnake");
+const Api = GramJS.Api
 const got = require('got');
+
 const youtubedl = require('youtube-dl-exec');
 
 const bot = new Snake({
@@ -49,7 +51,7 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => 
       .on('progress', p => console.log(p))
       .on('data', chunk => buffer.push(chunk))
       .on('end', async () => {
-        await ctx.telegram.sendMedia(ctx.chat.id,new InputMediaDocument,({
+        await ctx.telegram.sendMedia(ctx.chat.id,new Api.InputMediaDocument,({
           media: Buffer.concat(buffer),
           fileName: filename
         }))
