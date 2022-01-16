1850240e4fc9ceb096902bef6b2637188be4e4f9
BimoSora <bimosora3@gmail.com>
2022-01-16 08:08:13 +0700

    first commit

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ bot.command('yt', (ctx) => {
       preferFreeFormats: true,
       youtubeSkipDashManifest: true,
     }).then(async output => {
+        const filename = output.requested_formats[0].url.split('/').pop()
         await ctx.telegram.sendMessage(ctx.chat.id,`Upload start!`)
         const buffer = []
         const stream = got.stream(output.requested_formats[0].url)
@@ -99,7 +100,7 @@ bot.command('yt', (ctx) => {
         .on('data', chunk => buffer.push(chunk))
         .on('end', async () => {
           await ctx.telegram.sendDocument(ctx.chat.id,Buffer.concat(buffer),{
-            fileName : filename.mp4
+            fileName : filename
           })
           await ctx.telegram.sendMessage(ctx.chat.id,`Name: ${filename}`)
           await ctx.telegram.sendMessage(ctx.chat.id,`Upload successful`)
