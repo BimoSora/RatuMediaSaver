79ac8001079bd0be6b5722f62ac25605b8045f9a
BimoSora <bimosora3@gmail.com>
2022-01-26 12:04:42 +0700

    first commit

--- index.js
+++ index.js
@@ -97,7 +97,15 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,''),async (ctx) => 
       var regex3 = /\.[A-Za-z0-9]+$/gm
       var doctext3 = filename.replace(regex3, '');
       var doctext4 = filename.replace(regex3, 'null');
-      var photo = /^(([a-zA-Z]:)|(\\{2}\w+)\$?)(\\(\w[\w]*))+(jpeg|jpg|png|jpeg|gif|svg|webp)$/;
+
+      const mapObj = {
+        jpeg: 'jpeg',
+        jpg: 'jpg',
+        png: 'png',
+        gif: 'gif',
+        svg: 'svg'
+      };
+      var photo = filename.replace(/\b(?:jpeg|jpg|png|gif|svg)\b/gi, matched => mapObj[matched]);
 
   try{
 
