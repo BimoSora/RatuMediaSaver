1f8c84843f52235bfd79ad4979a3063a4b93ece6
BimoSora <bimosora3@gmail.com>
2022-01-27 10:18:58 +0700

    first commit

--- index.js
+++ index.js
@@ -97,13 +97,15 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,''),async (ctx) => 
       var regex3 = /\.[A-Za-z0-9]+$/gm
       var doctext3 = filename.replace(regex3, '');
       var doctext4 = filename.replace(regex3, 'null');
+      var photo = 'jpg' || 'png';
+      var photo2 = filename.replace(regex3, 'jpg' || 'png');
 
   try{
 
         if(doctext3 == doctext4){
           await ctx.telegram.sendMessage(ctx.chat.id,`Exstension not found`,{ replyToMsgId: message_id , parse_mode: 'Markdown'})
         }else{
-          if('jpg' != filename.replace(regex3, 'jpg') || 'png' != filename.replace(regex3, 'png') ){
+          if(photo != photo2){
             await ctx.telegram.sendMessage(ctx.chat.id,'Processing your file',{ replyToMsgId: message_id , parse_mode: 'Markdown'})
             const buffer = []
             const stream = got.stream(url)
