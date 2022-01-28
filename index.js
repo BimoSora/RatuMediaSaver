d74cff908f0580f975a75770f18b5e47f16b262b
BimoSora <bimosora3@gmail.com>
2022-01-28 10:43:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,''),async (ctx) => 
       var doctext3 = filename.replace(regex3, '');
       var doctext4 = filename.replace(regex3, 'null');
 
-      const words = filename.split('.');
+      const words = filename.split('.').pop()
       const words2 = words[1];
 
   try{
